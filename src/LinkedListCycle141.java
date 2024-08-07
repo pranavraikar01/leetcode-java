eaa96ebb7fda402cb4a8d708537571d15f26a225
PRANAV RAIKAR <pranavraikar01@gmail.com>
2024-08-07 11:11:58 +0530

    Added a different approach

--- src/LinkedListCycle141.java
+++ src/LinkedListCycle141.java
@@ -21,18 +21,29 @@
 
 public class LinkedListCycle141 {
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
+        // ListNode slow = head;
+        // ListNode fast = head;
 
-        while(slow != null && fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        // while(slow != null && fast != null && fast.next != null) {
+        //     slow = slow.next;
+        //     fast = fast.next.next;
 
-            if (slow == fast) {
+        //     if (slow == fast) {
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+        // Approach 2:
+        HashSet<ListNode> visited = new HashSet<>();
+        ListNode current = head;
+        while (current != null) {
+            if (visited.contains(current)) {
                 return true;
             }
+            visited.add(current);
+            current = current.next;
         }
-
         return false;
     }
 }
